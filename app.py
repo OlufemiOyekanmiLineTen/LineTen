cf751051ca230dc7ad4848c69cdf7a84e3042662
Olufemi Oyekanmi <OlufemiO@thomas.co>
2022-11-03 14:14:57 +0000

    Added Demo solution

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Linetem! It's Olufemi !"
+@app.route('/')
+def index():
+    return "Hello LineTen, it's Olufemi"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', port=81)
